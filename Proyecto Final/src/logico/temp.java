0ce5bb3991811d026211f78090ac5c5e4e158aa1
Robertmnz <Robertmnz@MuozV>
2020-03-05 14:54:44 -0400

    Cree set y get de la clase TEMP.

--- Proyecto Final/src/logico/temp.java
+++ Proyecto Final/src/logico/temp.java
@@ -1,5 +1,24 @@
 package logico;
 
 public class temp {
+private String Brayan; 
+private String Yehudy;
+public temp(String brayan, String yehudy) {
+	Brayan = brayan;
+	Yehudy = yehudy;
+}
+public String getBrayan() {
+	return Brayan;
+}
+public String getYehudy() {
+	return Yehudy;
+}
+public void setBrayan(String brayan) {
+	Brayan = brayan;
+}
+public void setYehudy(String yehudy) {
+	Yehudy = yehudy;
+} 
+
 
 }
